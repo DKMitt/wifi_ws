55cdcaed68af0f3604fc3fe1e2e299ff8b0b2368
DKMitt <dkmitt@sbcglobal.net>
2017-06-28 06:40:34 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React App</h2>
+          <h4>Welcome to the WiFi Weather Station</h4>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
