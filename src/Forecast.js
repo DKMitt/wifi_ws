3f6c9897d668d9329ec9927ca8726f06b78b4a72
DKMitt <dkmitt@sbcglobal.net>
2017-07-10 15:51:50 -0500

    content coding & css adjustment

--- src/Forecast.js
+++ src/Forecast.js
@@ -19,9 +19,9 @@ class Forecast extends Component {
 			        <h2>Forecast</h2>
 			        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum ex eget porttitor sollicitudin. Morbi cursus tempor placerat. Pellentesque suscipit tortor in orci pretium, ac facilisis ex pretium. Fusce hendrerit orci diam, vitae tristique quam porttitor eu. Donec ligula orci, ultricies in sagittis non, porta sed lorem. Aenean interdum posuere mattis. Curabitur dignissim dictum quam, vitae malesuada velit tristique a. </p>
 			        <div>
-			            <div className="more label"><a href="#">Read more</a></div> 
+			            <div className="more label"><a href="">Read more</a></div> 
 			            <div className="tags">
-			                <span className="btn-danger"><a href="#">energy</a></span><span className="btn-danger"><a href="#">reality</a></span><span className="btn-danger"><a href="#">world</a></span>
+			                <span className="btn-danger"><a href="/history">History</a></span><span className="btn-danger"><a href="/location">Location</a></span><span className="btn-danger"><a href="/forecast">Forecast</a></span>
 			            </div>    
 			        </div> 
   
